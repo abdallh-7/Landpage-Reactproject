5faa47a54f709d3b52ab547793c3104daca7973b
frgh11 <abdallahdomeno@gmail.com>
2025-12-17 16:21:04 +0200

    Fix component import case sensitivity

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react"
 import Navbar from "./components/Navbar";
 import Cards from "./components/Cards";
-import Hero from "./components/hero";
-import Footer from "./components/footer";
+import Hero from "./components/Hero";
+import Footer from "./components/Footer";
 function App() {
   const [darkMode,setDarkMode]=useState(true);
   return (
